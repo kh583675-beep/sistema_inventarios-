6a83618435461fed1e6860079bd30f5b89e3df44
kh583675-beep <kh583675@gmail.com>
2026-07-25 14:30:26 -0600

    Update Producto.java

---
+++ Producto.java
@@ -0,0 +1,16 @@
+public abstract class Producto {
+    protected String nombre;
+    protected double precio;
+    protected int stock;
+
+    public Producto(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public abstract double calcularPrecioFinal();
+    public abstract String obtenerCategoria();
+    public String getNombre() { return nombre; }
+    public double getPrecio() { return precio; }
+}
